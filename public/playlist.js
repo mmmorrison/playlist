0bbe574ea5ef78b00755537838b8b3092a53343f
Maria <mmorrison1978@gmail.com>
2015-12-09 15:47:42 -0700

    added images

--- public/playlist.js
+++ public/playlist.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+  $("#submitBtn").click(function() {
   comet = $.ajax({
     url: "https://lit-fortress-6467.herokuapp.com/object",
     method: "GET",
@@ -10,17 +10,15 @@ $(document).ready(function() {
   comet.done(function(payload) {
     console.log(payload);
 
-  $("#submitBtn").click(function() {
     var max = 2;
     var min = 0;
     var randy = Math.floor(Math.random() * (max - min + 1)) + min;
     var randImage = payload["results"][randy]["cover_art"];
-    $('#storeCovers').append("<img src='" + randImage + "'/>");
-    //
-    // $("#clearBtn").click(function() {
-    //   $('#storeCovers').remove();
+    $('.scroller').append("<img src='images/" + randImage + "'/>");
 
-    })
+    $("#clearBtn").click(function() {
+      $('.scroller').empty();
+
   })
 })
-// });
+})
