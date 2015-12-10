efddecdd3ac5789b5c5b4576a50601fb282cfc6e
Maria <mmorrison1978@gmail.com>
2015-12-10 08:50:27 -0700

    mod 3 updates

--- public/playlist.js
+++ public/playlist.js
@@ -9,47 +9,56 @@ $(document).ready(function() {
   comet.done(function(payload) {
     console.log(payload);
     // *********************POST REQUEST****************************
-    // comet = $.ajax({
+    //   comet = $.ajax({
     //   url: "https://lit-fortress-6467.herokuapp.com/post",
-    //   method: "POST",
-    //   data: {
-    //     foo: "bar"
-    //   }
+    //     method: "POST",
+    //     data: {
+    //       foo: "bar"
+    //     }
     // })
     // comet.done(function(payload) {
     //     console.log(payload + " post");
     //   })
-    // ***********************DOM MANIPULATIONS********************************
-    $(document).ready(function() {
-      payload["results"].forEach(function(e, i) {
-        var coverArt = payload["results"][i]["cover_art"];
-        var newId = "song" + i;
+    //***********************ADDS ALBUMS TO PLAYLIST PAGE********************************
 
-        $(".scroller").append('<div id='+newId+'>' + '<img src="images/' + coverArt + '"/>' + '</div>');
-      })
-    })
+    payload["results"].forEach(function(e, i) {
+      var album = payload["results"][i]["cover_art"];
+      var newId = "song" + i;
+      var addImage = '<img src="images/' + album + '"/>';
+      var albumTitle = payload["results"][i]["title"];
 
 
+      $(".scroller").append('<div id=' + newId + ' class="addName"' + '>' + addImage + '</div>');
 
-    var max = 2;
-    var min = 0;
-    var randy = Math.floor(Math.random() * (max - min + 1)) + min;
-    var randImage = payload["results"][randy]["cover_art"];
+      $(document).on("click", ".addName", function(e, i) {
 
-    $(".frontImg0").append("<img src='images/" + randImage + "'/>");
-    $(".frontImg1").append("<img src='images/" + randImage + "'/>");
-    $(".frontImg2").append("<img src='images/" + randImage + "'/>");
+        $("#storeNames").append(albumTitle + "<br>");
+
+        // e.preventDefault();
 
 
+        // ****************************IMAGES ON THE SPLASH PAGE*************************
+        var randy = Math.floor(Math.random() * 3);
+        var randImage = payload["results"][randy]["cover_art"];
 
-
-
-    $("#submitBtn").click(function() {
-
-      $("#clearBtn").click(function() {
-        $('.scroller').empty();
+        $(".frontImg0").append("<img src='images/" + randImage + "'/>");
+        $(".frontImg1").append("<img src='images/" + randImage + "'/>");
+        $(".frontImg2").append("<img src='images/" + randImage + "'/>");
 
       })
     })
   })
-});
+})
+
+
+
+
+
+
+//
+// $("#submitBtn").click(function() {
+//
+//   $("#clearBtn").click(function() {
+//     $('.scroller').empty();
+//   })
+//
