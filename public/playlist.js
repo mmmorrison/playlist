9ee49f13cb384327533b4c29e92646810bba404f
Maria <mmorrison1978@gmail.com>
2015-12-10 09:40:11 -0700

    updates

--- public/playlist.js
+++ public/playlist.js
@@ -1,3 +1,4 @@
+// *************************AJAX CALL GET REQUEST**************************
 $(document).ready(function() {
   comet = $.ajax({
     url: "https://lit-fortress-6467.herokuapp.com/object",
@@ -8,6 +9,7 @@ $(document).ready(function() {
   })
   comet.done(function(payload) {
     console.log(payload);
+
     // *********************POST REQUEST****************************
     //   comet = $.ajax({
     //   url: "https://lit-fortress-6467.herokuapp.com/post",
@@ -19,36 +21,34 @@ $(document).ready(function() {
     // comet.done(function(payload) {
     //     console.log(payload + " post");
     //   })
-    //***********************ADDS ALBUMS TO PLAYLIST PAGE********************************
 
+
+
+    // ***********************LOOPS OVER JSON*******************************
     payload["results"].forEach(function(e, i) {
-      var album = payload["results"][i]["cover_art"];
-      var newId = "song" + i;
-      var addImage = '<img src="images/' + album + '"/>';
-      var albumTitle = payload["results"][i]["title"];
-
-
-      $(".scroller").append('<div id=' + newId + ' class="addName"' + '>' + addImage + '</div>');
-
-      $(document).on("click", ".addName", function(e, i) {
-
-        $("#storeNames").append(albumTitle + "<br>");
-
-        // e.preventDefault();
-
-
-        // ****************************IMAGES ON THE SPLASH PAGE*************************
+        // *************************VARIABLES*******************************
+        var album = payload["results"][i]["cover_art"];
+        var newId = "song" + i;
+        var addImage = '<img src="images/' + album + '"/>';
+        var albumTitle = payload["results"][i]["title"];
         var randy = Math.floor(Math.random() * 3);
         var randImage = payload["results"][randy]["cover_art"];
-
+        // **********************ADDS IMAGES ON THE SPLASH PAGE*************************
         $(".frontImg0").append("<img src='images/" + randImage + "'/>");
         $(".frontImg1").append("<img src='images/" + randImage + "'/>");
         $(".frontImg2").append("<img src='images/" + randImage + "'/>");
+        //***********************ADDS IMAGES TO PLAYLIST PAGE****************************
+        $(".scroller").append('<div id=' + newId + ' class="addName"' + '>' + addImage + '</div>');
+        // *********************ADD ALBUM TITLE TO PLAYLIST DIV*****************************
+        $("#" + newId).on("click", function() {
+          $("#storeNames").append(albumTitle + "<br>");
+})
 
-      })
-    })
+
+})
   })
 })
+
 
 
 
